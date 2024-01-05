fc2feb82ffd9e2896f529658ce8336fe31457863
Benjamin Grosse <bjgrosse@gmail.com>
2024-01-05 10:31:24 +0100

    types.d.ts build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const food: Food = {
+    id: 1,
+    description: "apple",
+  };
+  console.log(food);
   return (
     <>
       <div>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
